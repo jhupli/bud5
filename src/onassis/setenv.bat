c726f057162d509c2c18f968773b029b3bf4ee5b
Janne Hupli <hupli@gvl.local>
2018-06-20 15:32:38 +0200

    skizze

---
+++ src/onassis/setenv.bat
@@ -0,0 +1,4 @@
+set DERBY_HOME=C:\Users\hupli\Google Drive\bud5\bud5\derby\db-derby-10.13.1.1-bin
+set PATH=%DERBY_HOME%\bin;%PATH%
+set CLASSPATH=C:\Users\hupli\git\bud5\src\onassis\target\onassis-5.0-SNAPSHOT.jar
+rem set Path=%DERBY_HOME%\bin;%Path%
---
+++ src/onassis/setenv.sh
@@ -0,0 +1,4 @@
+export DERBY_HOME=/Users/Janne/bud5/derby/db-derby-10.13.1.1-bin
+export PATH=$DERBY_HOME/bin:$PATH
+export CLASSPATH="/Users/Janne/Google Drive/bud5/bud5/src/onassis/target/onassis-5.0-SNAPSHOT.jar"			
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`

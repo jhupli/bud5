dc34f7ec1d35fe8c1cef7f81b99b32f27f160a5a
Janne <Janne@jannes-mbp>
2019-01-03 08:33:22 +0100

    removed console debugs

--- src/onassis/react/onassis/src/components/minibars/minibars.js
+++ src/onassis/react/onassis/src/components/minibars/minibars.js
@@ -7,40 +7,9 @@ import '../../../node_modules/c3/c3.min.css'
 import { connect } from 'react-redux'
 import { minibars } from './minibars_d3'
 
-//var minibars = require('./minibars_d3')
-/*var i=0;
-
-function dataprovider(cb) {
-	  console.log("dataprovider called")
-
-	  axios.get('http://localhost:8080/calc', {		  
-		  port: 8080,
-		  params: {
-			  left: 12345,
-			  right: 5678
-		  }
-	  })
-	  .then(function (response) {
-
-		  console.log(response);
-		  var ret = {
-		    	  	  "data" : response.data,
-		    	  	  balance : 0
-		  }
-		  cb(ret)
-	  })
-	  .catch(function (error) {
-
-		  console.log(error);
-	  });
-}*/
-
-
-
 class Minibars extends React.Component {
   constructor(props) {
     super(props);
-  //  console.log("***************************** constructor")
     this.state = {
     		start: new Date(),
     		end: new Date(),
@@ -48,19 +17,14 @@ class Minibars extends React.Component {
     }
     // This binding is necessary to make `this` work in the callback
     this.select = this.select.bind(this);
-  
-    //this.dataprovider = dataprovider.bind(this);
-    props.load();
+      props.load();
   }
   
   componentDidMount() {
-//	  console.log("***************************** componentDidMount "+ this.props.name)
 	  var minibarconfig = {
 			  	max_selection_days: 400,
 			    bindto: this.props.name,
 			    select: this.select,
-			    //dataprovider: this.dataprovider
-			    //dataprovider: dataprovider
 			}  
 	  
 	  this.minibars = minibars;
@@ -68,23 +32,17 @@ class Minibars extends React.Component {
   }
   
   select(start, end) {
-	  console.log("***************************** select ")
 	  this.su = false;
       this.props.setDateRange(new Date(start), new Date(end), "rangePicker1")
   }
   
   componentWillReceiveProps(nextProps){
-	  console.log("***************************** componentWillReceiveProps ")
-//	  console.log(nextProps)
 	  this.minibars.generate(nextProps.balances)
 	  this.minibars.select(nextProps.start, nextProps.end)
 	  
   }
 
   render() {
-	 
-	 console.log("***************************** render ")
-  
     return (
     	<div>
 	        <div id={this.props.name} />

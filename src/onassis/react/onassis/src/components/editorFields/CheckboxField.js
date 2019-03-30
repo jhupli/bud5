ae73e470921e9248db7d68ca53bf00d8b6d87d72
Janne Hupli <Janne@jannes-mbp.speedport.ip>
2019-03-30 10:17:27 +0100

    styling

--- src/onassis/react/onassis/src/components/editorFields/CheckboxField.js
+++ src/onassis/react/onassis/src/components/editorFields/CheckboxField.js
@@ -14,6 +14,7 @@ class CheckboxField extends React.Component {
 						disabled = {readOnly}
 					    id = {id}  
 						checked = {checked}
+
 						onChange = {(e) => onValueChanged(e.target.checked, field, index)}
 						style = { readOnly ? {backgroundColor: 'lightgray', borderColor: 'lightgray'} : 
 							         (touched ? {backgroundColor: 'darkKhaki', borderColor: 'darkKhaki'} : {})}

bc32f8d7b835a0c357b1fb59ac736adfef239626
Janne Hupli <Janne@jannes-mbp.speedport.ip>
2019-06-23 10:52:57 +0200

    PRE-6.6

---
+++ src/onassis/src/main/resources/public/precache-manifest.a3851496ab7af58f473238f896ea1620.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "357f90b213f246b29828",
+    "url": "/static/css/main.5b6e584d.chunk.css"
+  },
+  {
+    "revision": "357f90b213f246b29828",
+    "url": "/static/js/main.357f90b2.chunk.js"
+  },
+  {
+    "revision": "f9d995bd574d64c32d83",
+    "url": "/static/css/1.65409cfc.chunk.css"
+  },
+  {
+    "revision": "f9d995bd574d64c32d83",
+    "url": "/static/js/1.f9d995bd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb8b00624f4979468f6a3c8342f1b42c",
+    "url": "/index.html"
+  }
+];

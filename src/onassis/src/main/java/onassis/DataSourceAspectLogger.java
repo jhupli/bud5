98e32c4ed85fb218a8e3e4e1dcbbd5fad2637726
Janne Hupli <jhupli@gmail.com>
2019-10-28 10:31:54 +0000

    CHG-105

--- src/onassis/src/main/java/onassis/DataSourceAspectLogger.java
+++ src/onassis/src/main/java/onassis/DataSourceAspectLogger.java
@@ -9,8 +9,10 @@ import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("dev")
 @Aspect
 @Component
 public class DataSourceAspectLogger {

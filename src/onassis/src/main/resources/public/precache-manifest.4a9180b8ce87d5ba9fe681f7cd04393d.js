c0ca4727f70b53dce2171dcea434506cff9eb251
Janne Hupli <jhupli@gmail.com>
2019-11-10 09:31:17 +0100

    PRE 5.7

---
+++ src/onassis/src/main/resources/public/precache-manifest.4a9180b8ce87d5ba9fe681f7cd04393d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "10da768cd55b9d82bf2c",
+    "url": "/static/js/main.10da768c.chunk.js"
+  },
+  {
+    "revision": "0d5451d3d721c4b72941",
+    "url": "/static/js/1.0d5451d3.chunk.js"
+  },
+  {
+    "revision": "10da768cd55b9d82bf2c",
+    "url": "/static/css/main.219c8641.chunk.css"
+  },
+  {
+    "revision": "0d5451d3d721c4b72941",
+    "url": "/static/css/1.e5ede3a7.chunk.css"
+  },
+  {
+    "revision": "3356fc07b33440240c01274270220347",
+    "url": "/index.html"
+  }
+];

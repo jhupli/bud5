b09eb9e0db99cb70f302d1fc506c840441951fdb
Janne Hupli <jhupli@gmail.com>
2019-11-10 09:21:42 +0100

    Maven problem fixed (corrupted font, loginerror)

--- src/onassis/src/main/java/onassis/SecurityConfig.java
+++ src/onassis/src/main/java/onassis/SecurityConfig.java
@@ -1,14 +1,17 @@
 package onassis;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
- 
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+//@Profile("prod")
 @Configuration
-@Profile("prod")
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
     @Override

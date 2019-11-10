587f1e6cd13d48f2aee3208439783717a18d7990
Janne Hupli <jhupli@gmail.com>
2019-11-10 07:15:57 +0100

    CHG-89

--- src/onassis/react/onassis/src/components/main/Onassis.js
+++ src/onassis/react/onassis/src/components/main/Onassis.js
@@ -1,11 +1,28 @@
 import React from 'react' // ← Main React library
+import KeyboardEventHandler from 'react-keyboard-event-handler';
 import Content from './Content'
 import ErrorMessage from './ErrorMessage'
 import MenuBar from '../navi/MenuBar'
 import Ping from '../ping/ping'
 import { connect } from 'react-redux'
-import { today } from '../../actions/day'
+import { today, prev_day, next_day } from '../../actions/day'
+import { daterange_prev_block, daterange_next_block } from '../../actions/daterange'
+
 //CHG-10 import { set_daterange } from '../../actions/daterange'
+
+const Keydetect = (props) => (<div>
+  <KeyboardEventHandler
+    handleKeys={['shift+left', 'left', 'shift+right', 'right']}
+    onKeyEvent={(key, e) => {
+        switch(key) {
+            case 'shift+left' : props.daterange_prev_block(); break;
+            case 'left' : props.prev_day(); break;
+            case 'shift+right' : props.daterange_next_block(); break;
+            case 'right' : props.next_day(); break;
+        }
+        }}
+    />
+</div>);
 
 class Onassis extends React.Component{
 	componentDidMount() {
@@ -16,6 +33,11 @@ class Onassis extends React.Component{
 	render(){
 	    return (
 	    	<div>
+	    	 <Keydetect
+	    	    daterange_prev_block={this.props.daterange_prev_block}
+	    	    daterange_next_block={this.props.daterange_next_block}
+	    	    prev_day={this.props.prev_day}
+	    	    next_day={this.props.next_day}/>
 	    	 <Ping />
 	    	 <MenuBar />
 	    	 <ErrorMessage />
@@ -32,7 +54,20 @@ function mapDispatchToProps(dispatch) {
     return ({
         today: () => {
             dispatch(today())
-        }/* //CHG-10 ,
+        },
+        prev_day: () => {
+            dispatch(prev_day())
+        },
+        next_day: () => {
+            dispatch(next_day())
+        },
+        daterange_prev_block: () => {
+                    dispatch(daterange_prev_block())
+                },
+        daterange_next_block: () => {
+            dispatch(daterange_next_block())
+        },
+        /* //CHG-10 ,
         setDateRange: (start, end) => {
             dispatch(set_daterange(start, end))
         }*/

736b46399f14772615d6856edbc294cc654f6f4c
Janne Hupli <janne.hupli@data-experts.de>
2020-01-24 09:47:09 +0100

    CHG-109

--- src/onassis/react/onassis/src/components/paymentsEditor/recurrent.js
+++ src/onassis/react/onassis/src/components/paymentsEditor/recurrent.js
@@ -4,21 +4,30 @@ import {isEven} from '../../util/math'
 
 var dateFormat = require('dateformat')
 
+function _addDays(d, days) {
+    var date = toDateFi(d)
+    var date1 = addDays(date, days)
+    return dateFormat(date1, "dd.mm.yyyy")
+}
+
+function _addMonths(d, months) {
+  var date = toDateFi(d)
+  var date1 = addMonths(date, months)
+  return dateFormat(date1, "dd.mm.yyyy")
+}
+
 var f = {
 	"DAILY" : (payment, iteration) => {
-		var date = toDateFi(payment.d)
-		var date1 = addDays(date, 1)
-		payment.d = dateFormat(date1, "dd.mm.yyyy")
+    payment.d = _addDays(payment.d, 1)
+    payment.dc = _addDays(payment.dc, 1)
 	},
 	"WEEKLY" : (payment, iteration) => {
-		var date = toDateFi(payment.d)
-		var date1 = addDays(date, 7)
-		payment.d = dateFormat(date1, "dd.mm.yyyy")
+    payment.d = _addDays(payment.d, 7)
+    payment.dc = _addDays(payment.dc, 7)
 	},
 	"FORTNIGHTLY" : (payment, iteration) => {
-		var date = toDateFi(payment.d)
-		var date1 = addDays(date, 14)
-		payment.d = dateFormat(date1, "dd.mm.yyyy")
+    payment.d = _addDays(payment.d, 14)
+    payment.dc = _addDays(payment.dc, 14)
 	},
 	"TWICE_A_MONTH" : (payment, iteration) => {
 		var date = toDateFi(payment.d)
@@ -30,36 +39,40 @@ var f = {
 			date1 = addMonths(date1, 1)
 		}
 		payment.d = dateFormat(date1, "dd.mm.yyyy")
+
+    date = toDateFi(payment.dc)
+    date1 = null
+    if(isEven(iteration)) {
+      date1 = addDays(date, 14)
+    } else {
+      date1 = addDays(date, -14)
+      date1 = addMonths(date1, 1)
+    }
+    payment.dc = dateFormat(date1, "dd.mm.yyyy")
 	},
 	"MONTHLY" : (payment, iteration) => {
-		var date = toDateFi(payment.d)
-		var date1 = addMonths(date, 1)
-		payment.d = dateFormat(date1, "dd.mm.yyyy")
+		payment.d = _addMonths(payment.d, 1)
+    payment.dc = _addMonths(payment.dc, 1)
 	},
 	"EVERY_OTHER_MONTH" : (payment, iteration) => {
-		var date = toDateFi(payment.d)
-		var date1 = addMonths(date, 2)
-		payment.d = dateFormat(date1, "dd.mm.yyyy")
+    payment.d = _addMonths(payment.d, 2)
+    payment.dc = _addMonths(payment.dc, 2)
 	},
 	"EVERY_3_MONTHS" : (payment, iteration) => {
-		var date = toDateFi(payment.d)
-		var date1 = addMonths(date, 3)
-		payment.d = dateFormat(date1, "dd.mm.yyyy")
+    payment.d = _addMonths(payment.d, 3)
+    payment.dc = _addMonths(payment.dc, 3)
 	},
 	"EVERY_4_MONTHS" : (payment, iteration) => {
-		var date = toDateFi(payment.d)
-		var date1 = addMonths(date, 4)
-		payment.d = dateFormat(date1, "dd.mm.yyyy")
-	},
+    payment.d = _addMonths(payment.d, 4)
+    payment.dc = _addMonths(payment.dc, 4)
+  },
 	"EVERY_6_MONTHS" : (payment, iteration) => {
-		var date = toDateFi(payment.d)
-		var date1 = addMonths(date, 6)
-		payment.d = dateFormat(date1, "dd.mm.yyyy")
+    payment.d = _addMonths(payment.d, 6)
+    payment.dc = _addMonths(payment.dc, 6)
 	},
 	"YEARLY" : (payment, iteration) => {
-		var date = toDateFi(payment.d)
-		var date1 = addMonths(date, 12)
-		payment.d = dateFormat(date1, "dd.mm.yyyy")
+    payment.d = _addMonths(payment.d, 12)
+    payment.dc = _addMonths(payment.dc, 12)
 	}
 }
 

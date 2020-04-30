6d6dbdee5668600c73e30dfdf72cbfc383dad620
Janne Hupli <jhupli@gmail.com>
2020-04-30 10:07:57 +0200

    PRE6.1

---
+++ src/onassis/src/main/resources/public/precache-manifest.16f510a156b2adb5737f73baeb2d3491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "938846bb5edbbaa9e248",
+    "url": "/static/js/main.938846bb.chunk.js"
+  },
+  {
+    "revision": "21cc0f148fd981be6ada",
+    "url": "/static/js/1.21cc0f14.chunk.js"
+  },
+  {
+    "revision": "938846bb5edbbaa9e248",
+    "url": "/static/css/main.ec89c7be.chunk.css"
+  },
+  {
+    "revision": "21cc0f148fd981be6ada",
+    "url": "/static/css/1.cd7c1325.chunk.css"
+  },
+  {
+    "revision": "12a9a601c05bf83a8a7346082fae6cb7",
+    "url": "/index.html"
+  }
+];
--- src/onassis/src/main/resources/public/service-worker.js
+++ src/onassis/src/main/resources/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.546096bc680fc7b63bc911f6e1c9a423.js"
+  "/precache-manifest.16f510a156b2adb5737f73baeb2d3491.js"
 );
 
 workbox.clientsClaim();

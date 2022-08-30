cffad79aec2575b2b8d09094b017d42796677515
jhupl <jhupli@gmail.com>
2022-08-30 06:32:43 +0200

    worksave

--- src/utils/src/main/java/onassis/utils/payment/synchronizer/parsers/Parser.java
+++ src/utils/src/main/java/onassis/utils/payment/synchronizer/parsers/Parser.java
@@ -1,13 +1,16 @@
 package onassis.utils.payment.synchronizer.parsers;
 
+import lombok.SneakyThrows;
 import org.apache.commons.collections.map.HashedMap;
 
+import java.io.InputStream;
 import java.util.*;
 import java.util.stream.Stream;
 
+
 public class Parser {
    public enum Parsing{
-        BERGIN("begin_rexp"),
+        BEGIN("begin_rexp"),
         DAY("day_rexp"),
         MONTH("month_rexp"),
         YEAR("year_rexp"),
@@ -36,13 +39,23 @@ public class Parser {
         public static Stream<Skipping> stream() {
             return Stream.of(Skipping.values());
         }
-
-        
     }
+
     public final String SKIP="skip";
-    private final Map<Parsing, PartialParser> parser = new HashMap<>();
+    private final Map<Parsing, PartialParser> parsers = new HashMap<>();
 
-
-
-
+    @SneakyThrows
+    public Parser(InputStream inputStream) {
+        PropertiesExt _properties = new PropertiesExt();
+        _properties.load(inputStream);
+        Parsing.stream().map( p -> parsers.put(p, new PartialParser(_properties.getStringArray(p.regexpName))) );
+        if(null == parsers.get(Parsing.BEGIN)) {
+            throw new IllegalArgumentException("Empty regexps!");
+        }
+        parsers.values().forEach(v -> {
+            if (v.length() != parsers.get(Parsing.BEGIN).length()) {
+                throw new IllegalArgumentException("All regexps must have same nubmer of indexes!");
+            }}
+        );
+    }
 }
